09b867d89e2e199b3b9e2e79ef36a8c537099d15
Oscary Garcia <oscarygarcia@tecnologiateradata15.com>
2020-10-05 18:02:59 -0400

    Create promise

---
+++ src/promise/index.js
@@ -0,0 +1,38 @@
+const somethingWillHappen = () => {
+  return new Promise((resolve, reject) => {
+    if (true) {
+      resolve("Hey! cool");
+    } else {
+      reject("Salió mal...");
+    }
+  });
+};
+
+somethingWillHappen()
+  .then((response) => console.log(response))
+  .catch((err) => console.log(err));
+
+const somethingWillHappenSecond = () => {
+  return new Promise((resolve, reject) => {
+    if (true) {
+      setTimeout(() => {
+        resolve("el 2do es este");
+      }, 200);
+    } else {
+      const error = new Error("FALLO!!");
+      reject(error);
+    }
+  });
+};
+
+somethingWillHappenSecond()
+  .then((response) => console.log(response))
+  .catch((err) => console.log(err));
+
+Promise.all([somethingWillHappen(), somethingWillHappenSecond()])
+  .then((response) => {
+    console.log("Array of result", response);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
