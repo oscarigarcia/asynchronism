fa519123e513d217af89d27d11dd72637144c7fb
Oscary Garcia <oscarygarcia@tecnologiateradata15.com>
2020-10-06 13:41:03 -0400

    Add challenge promise ecmas6

---
+++ src/promise/challenge.js
@@ -0,0 +1,18 @@
+const API = "https://rickandmortyapi.com/api/character/";
+const fetchData = require("../ultis/fetchData");
+
+fetchData(API)
+  .then((data) => {
+    console.log(data.info.count);
+    return fetchData(`${API}${data.results[0].id}`);
+  })
+  .then((data) => {
+    console.log(data.name);
+    return fetchData(data.origin.url);
+  })
+  .then((data) => {
+    console.log(data.dimension);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
